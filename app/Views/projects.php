be701e5bcfeffdc95c161161e6f6f11cf71cf64a
James Cuadra <jjlcuadradev@gmail.com>
2025-01-28 19:18:52 +0800

    Adjusted html structure of projects page

--- app/Views/projects.php
+++ app/Views/projects.php
@@ -15,8 +15,8 @@
     <main>
         <div class="projects-section" id="projects-section">
             <h1 id="title-slide">Projects</h1>
-            <?php foreach ($projects as $project): ?>
             <ul class="projects-container">
+                <?php foreach ($projects as $project): ?>
                 <li class="rev-block" id="reveal-animation">
                     <div class="project-content" id="reveal-opacity-animation">
                         <p><?= $project['project_name'] ?></p>
@@ -24,8 +24,8 @@
                         <a href="<?= $project['project_link'] ?>" target="_blank">View the Site Here</a>
                     </div>
                 </li>
+                <?php endforeach?>
             </ul>
-            <?php endforeach?>
         </div>
     </main>
 </body>
